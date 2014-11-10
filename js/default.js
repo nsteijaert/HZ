62a62e86914bcccf74a8de34e210bed9e185ce9e
PtrMns <pietermoens@outlook.com>
2014-11-10 21:49:56 +0100

    Some basic things...

---
+++ js/default.js
@@ -0,0 +1,29 @@
+$(function() {
+	$('form').submit(function() {
+		var query = $('#query').val();
+		var result = $('#selection').val();
+
+		if (query != "") {
+			$.ajax({
+				type : "POST",
+				cache : false,
+				dataType : "json",
+				url : "load.php",
+				async : false,
+				data : {
+					query : query
+				}
+			}).done(function(result) {
+				console.log("Data successfully retrieved...");
+				$('.result').html(result);
+			}).fail(function() {
+				console.log("Error retrieving data...");
+			});
+
+			console.log("Asked query: " + query);
+		} else {
+			console.log("No given query");
+		}
+		return false;
+	});
+});
