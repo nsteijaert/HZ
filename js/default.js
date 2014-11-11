d53ac8d6d945fbac46fa1c07b330805faef50ac5
PtrMns <pietermoens@outlook.com>
2014-11-11 10:01:37 +0100

    Nice json

--- js/default.js
+++ js/default.js
@@ -15,7 +15,7 @@ $(function() {
 				}
 			}).done(function(result) {
 				console.log("Data successfully retrieved...");
-				$('.result').html(result);
+				$('.result').html("<pre>" + syntaxHighlight(JSON.stringify(result, undefined, 4))+ "</pre>");
 			}).fail(function() {
 				console.log("Error retrieving data...");
 			});
@@ -27,3 +27,25 @@ $(function() {
 		return false;
 	});
 });
+
+/*
+ * Show the json string with nice syntax.
+ */
+function syntaxHighlight(json) {
+	json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function(match) {
+		var cls = 'number';
+		if (/^"/.test(match)) {
+			if (/:$/.test(match)) {
+				cls = 'key';
+			} else {
+				cls = 'string';
+			}
+		} else if (/true|false/.test(match)) {
+			cls = 'boolean';
+		} else if (/null/.test(match)) {
+			cls = 'null';
+		}
+		return '<span class="' + cls + '">' + match + '</span>';
+	});
+}
--- php/SPARQLClient.php
+++ php/SPARQLClient.php
@@ -1,3 +1,20 @@
 <?php
-    phpinfo();
+echo '{
+	"title": "Example Schema",
+	"type": "object",
+	"properties": {
+		"firstName": {
+			"type": "string"
+		},
+		"lastName": {
+			"type": "string"
+		},
+		"age": {
+			"description": "Age in years",
+			"type": "integer",
+			"minimum": 0
+		}
+	},
+	"required": ["firstName", "lastName"]
+}';
 ?>
