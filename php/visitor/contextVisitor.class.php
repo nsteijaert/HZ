63d74527affbb299f2d5255b58c778e077457cac
nsteijaert <nick@steijaert.org>
2014-11-14 10:00:47 +0100

    Formatted code #2

---
+++ php/visitor/contextVisitor.class.php
@@ -0,0 +1,17 @@
+<?php
+class contextVisitor extends Visitor {
+    private $value1 = null;
+    function getValue1() {
+        return $this -> value1;
+    }
+
+    function setValue1($value1_in) {
+        $this -> value1 = $value1_in;
+    }
+
+    function visitContext(ContextVisitee $contextVisiteeIn) {
+        $this -> setValue1($contextVisiteeIn -> getValue1());
+    }
+
+}
+?>
