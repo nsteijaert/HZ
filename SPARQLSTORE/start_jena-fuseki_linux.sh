3440a40bd8f2810e8c0b55c209a48e2e04a5986f
Gymnasiast <m.o.steenbeek@gmail.com>
2014-11-18 13:51:52 +0100

    Script voor Linux

---
+++ SPARQLSTORE/start_jena-fuseki_linux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "/home/michael/Documenten/HZ/Jaar 4/Afstuderen/Nieuw/Sparql"
+./fuseki-server -update -port=3030 -loc="/home/michael/Documenten/HZ/Jaar 4/Afstuderen/Nieuw/Sparql/ds" /ds
