d31ed5a6ed409e74e45e2a2a5ae27c80e1797f66
nsteijaert <nick@steijaert.org>
2014-11-20 10:10:14 +0100

    Added comments and minor bugfixes

--- php/SPARQLObject.class.php
+++ php/SPARQLObject.class.php
@@ -4,29 +4,35 @@
  * SPARQLObject is part of the intermediate datastructure, this class instantiates an object with an array of properties.
  */
 class SPARQLObject {
-  
+
     //Global Variables
     private $type;
     private $properties = array();
-    
+
+    //Constructor for a SPARQLObject
     public function __construct($type_in) {
-        $this->type = $type_in;
+        $this -> type = $type_in;
     }
-    
+
+    //Sets a property
     public function setProperty($property_name, $property_value) {
-        $properties[$property_value] = $property_name;
+        $this -> $properties[$property_value] = $property_name;
     }
-    
+
+    //Sets the type
     public function getType() {
-        return $this->$type;
+        return $this -> $type;
     }
-    
+
+    //Return a property with a given key
     public function getProperty($value) {
-        return $properties[$value];
+        return $this -> $properties[$value];
     }
-    
+
+    //Return the array with properties
     public function getAllProperties() {
-        return $this->$properties;
+        return $this -> $properties;
     }
-}  
+
+}
 ?>
