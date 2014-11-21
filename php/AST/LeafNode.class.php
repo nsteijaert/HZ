6e3aee635c83b210dc344ed8afd0b9e98f5b095e
nsteijaert <nick@steijaert.org>
2014-11-21 11:40:58 +0100

    Non-existing classes now exist as non-existing classes

---
+++ php/AST/LeafNode.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * LeafNode is part of a abstract syntax tree pattern used for parsing SPARQL data.
+ */
+class LeafNode extends ASTObject{
+
+    public function __construct() {
+        
+    }
+    
+    
+}
+?>
