42246ef6bcd703afbd1c03f0c164482832b28b8c
nsteijaert <nick@steijaert.org>
2014-11-21 11:05:47 +0100

    edit

---
+++ php/AST/containerNode.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ContainerNode is part of a abstract syntax tree pattern used for parsing SPARQL data.
+ */
+class ContainerNode {
+
+    public function __construct() {
+        
+    }
+    
+    
+}
+?>
