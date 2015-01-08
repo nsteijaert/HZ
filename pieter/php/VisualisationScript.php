4828c21f5ef4dd2caf561faadfaac48e6be9f091
PtrMns <pietermoens@outlook.com>
2015-01-08 12:08:46 +0100

    Solved zoom functionality.

--- pieter/php/VisualisationScript.php
+++ pieter/php/VisualisationScript.php
@@ -3,26 +3,21 @@ include_once (__DIR__ . '/QueryBuilder.class.php');
 include_once (__DIR__ . '/DataParser.class.php');
 include_once (__DIR__ . '/visitor/NodeMapVisitor.class.php');
 
-switch ($_POST["do"]) {
-	case "generate" :
-		// Load data
-		$querybuilder = new QueryBuilder($_POST["depth"], $_POST["concept"]);
-		echo $query = $querybuilder -> generateQuery($_POST["relations"]);
+// Load data
+$querybuilder = new QueryBuilder($_POST["depth"], $_POST["concept"]);
+$query = $querybuilder -> generateQuery($_POST["relations"]);
+$result = file_get_contents('http://localhost:3030/ds/query?output=json&query=' . urlencode($query));
 
-		break;
-	case "parse" :
-		// Parse data
-		$parser = new DataParser($_POST["data"]);
-		$objects = $parser -> parseDataRDF();
+// Parse data
+$parser = new DataParser(json_decode($result, true));
+$objects = $parser -> parseDataRDF();
 
-		// Handle data
-		$visitor = new NodeMapVisitor();
-		foreach ($objects as $object) {
-			$object -> accept($visitor);
-		}
-		echo $visitor -> getUsableJSON();
-		break;
-	default :
-		exit ;
+// Handle data
+$visitor = new NodeMapVisitor();
+foreach ($objects as $object) {
+	$object -> accept($visitor);
 }
+
+// Return JSON
+echo $visitor -> getUsableJSON();
 ?>
