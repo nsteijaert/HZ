dac3709b8cc851a8c582aac5a7f5546925db80ac
Gymnasiast <m.o.steenbeek@gmail.com>
2015-03-05 10:41:27 +0100

    Ondersteuning prefixes in verbinding, url-functies gerefactord naar aparte klasse

--- php/SPARQLConnection.class.php
+++ php/SPARQLConnection.class.php
@@ -1,24 +1,47 @@
 <?php
+require_once(__DIR__.'/Uri.class.php');
+
 class SPARQLConnection
 {
 	private $default_endpoint="http://127.0.0.1:3030/ds/query?output=json&query=";
 	private $endpoint="";
-	
+	private $default_prefixes=array();
+
 	public function __construct($endpoint=null)
 	{
 		if ($endpoint==null)
 			$this->endpoint=$this->default_endpoint;
+		$this->default_prefixes[]='rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>';
+		$this->default_prefixes[]='rdfs: <http://www.w3.org/2000/01/rdf-schema#>';
+		$this->default_prefixes[]='owl: <http://www.w3.org/2002/07/owl#>';
+		$this->default_prefixes[]='swivt: <http://semantic-mediawiki.org/swivt/1.0#>';
+		$this->default_prefixes[]='xsd: <http://www.w3.org/2001/XMLSchema#>';
+
+		$this->default_prefixes[]='property: <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3A>';
+		$this->default_prefixes[]='emmwiki: <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/>';
 	}
-	
+
 	public function JSONQuery($query)
 	{
-		return file_get_contents($this->endpoint.urlencode($query));
+		$prefixed_query=self::prefixesIntoQuery().$query;
+		return Uri::codeerHaakjes(file_get_contents($this->endpoint.rawurlencode($prefixed_query)));
 	}
-	
+
 	public function JSONQueryAsPHPArray($query)
 	{
 		$json=self::JSONQuery($query);
 		return json_decode($json,true);
+	}
+
+	public function prefixesIntoQuery($extra_prefixes=array())
+	{
+		$prefixes=array_merge($this->default_prefixes,$extra_prefixes);
+
+		foreach($prefixes as $prefix)
+		{
+			$return.='PREFIX '.$prefix." \n";
+		}
+		return $return;
 	}
 }
 ?>
---
+++ php/Uri.class.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Diverse functies voor het manipuleren van uri's.
+ */
+ class Uri
+ {
+ 	// Alles is statisch, dus een constructor is niet nodig.
+ 	private function __construct() {}
+	
+	public static function codeerHaakjes($string)
+	{
+		return strtr($string,array('('=>'%28',')'=>'%29'));
+	}
+	
+	public static function decodeerHaakjes($string)
+	{
+		return strtr($string,array('%28'=>'(', '%29'=>')'));
+	}
+
+	/**
+	 * Voorziet een uri van vishaken ('<' en '>'), indien niet aanwezig.
+	 */	
+	public static function escape_uri($uri)
+	{
+		if (substr($uri,0,4)=='http')
+		{
+			return '<'.$uri.'>';
+		}
+		else
+		{
+			return $uri;
+		}
+	}
+	
+	/**
+	 * Verwijdert de vishaken ('<' en '>') van de uri, indien aanwezig.
+	 */
+	public static function deescape_uri($uri)
+	{
+		if (substr($uri,0,1)=='<')
+		{
+			return substr($uri,1,-1);
+		}
+		else
+		{
+			return $uri;
+		}
+	}
+	
+	/**
+	 * Zet een in SMW opgeslagen naam van een Mediawiki-artikel om in een leesbare variant.
+	 * Bijvoorbeeld: Eigenschap-3AElement_link_type naar Eigenschap: Element link type
+	 */
+	public static function decodeerSMWNaam($naam)
+	{
+		// ID's in RDF-store staan in procentnotatie, maar met een - ipv een %. Dit stukje code zorgt voor
+		// een correcte procentnotatie en zet die vervolgens om naar de bedoelde tekens.
+		return urldecode(strtr($naam, "-_","% "));
+	}
+
+	/**
+	 * Zet een SMW-uri om in een voor mensen leesbare titel.
+	 * Bijvoorbeeld: 'http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Building_with_Nature-2Dinterventies_op_het_systeem'
+	 * Naar: 'Building with Nature-interventies op het systeem'.
+	 */
+	public static function SMWuriNaarLeesbareTitel($uri)
+	{
+		$name_array=preg_split("/URIResolver\//", self::decodeerSMWNaam($uri));
+		return $name_array[1];
+	}
+ }
+ 
+?>
