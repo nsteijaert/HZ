c58743be759187d9f5f59ae1d10640ef0165891a
Gymnasiast <m.o.steenbeek@gmail.com>
2015-03-05 11:35:05 +0100

    Ondersteuning prefixes, gerefactord, Connects-verbanden gefixt.

--- php/php-emont/JSON_EMontParser.class.php
+++ php/php-emont/JSON_EMontParser.class.php
@@ -8,6 +8,8 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 require_once(__DIR__.'/../SPARQLConnection.class.php');
+require_once(__DIR__.'/../Uri.class.php');
+
 require_once(__DIR__.'/Context.class.php');
 require_once(__DIR__.'/IntentionalElement.class.php');
 require_once(__DIR__.'/Activity.class.php');
@@ -35,12 +37,14 @@ class JSON_EMontParser
 
 		$subrollen=self::zoekSubrollen($this->situatie_uri);
 
-		$alle_te_doorzoeken_uris=array_merge(array($this->situatie_uri),$subrollen);
-		//var_dump($alle_te_doorzoeken_uris);
-		$zoekstring=implode('> } UNION { ?ie <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3AContext> <',$alle_te_doorzoeken_uris);
+		foreach(array_merge(array(Uri::escape_uri($this->situatie_uri)),$subrollen) as $te_doorzoeken_uri)
+		{
+			$alle_te_doorzoeken_uris[]=Uri::escape_uri($te_doorzoeken_uri);
+		}
 
-		$query_inhoud_situatie='DESCRIBE ?ie WHERE {{ ?ie <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3AContext> <'.$zoekstring.'> }.{?ie <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Categorie-3AIntentional_Element>} UNION {?ie <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Categorie-3AActivity>}}';
-		//echo '<pre>'.$connectie->JSONQuery($query_inhoud_situatie).'</pre>';
+		$zoekstring=implode(' } UNION { ?ie property:Context ',$alle_te_doorzoeken_uris);
+
+		$query_inhoud_situatie='DESCRIBE ?ie WHERE {{ ?ie property:Context '.$zoekstring.' }.{?ie rdf:type <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Categorie-3AIntentional_Element>} UNION {?ie rdf:type <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Categorie-3AActivity>}}';
 
 		$data=$connectie->JSONQueryAsPHPArray($query_inhoud_situatie);
 
@@ -51,116 +55,102 @@ class JSON_EMontParser
 		$instanceOf=array();
 		$partOf=array();
 
-		foreach ($data['@graph'] as $item)
+		if($data['@graph'])
 		{
-			// Bepaal type IE
-			switch($item['Eigenschap-3AIntentional_Element_type'])
+			foreach ($data['@graph'] as $item)
 			{
-				case 'Activity':
-					$obj=new Activity($item['@id']);
-					break;
-				case 'Outcome':
-					$obj=new Outcome($item['@id']);
-					break;
-				case 'Goal':
-					$obj=new Goal($item['@id']);
-					break;
-				case 'Belief':
-					$obj=new Belief($item['@id']);
-					break;
-				case 'Condition':
-					$obj=new Condition($item['@id']);
-					break;
-				default:
-					$obj = new IntentionalElement($item['@id']);
-			}
-
-			if($item['Eigenschap-3AHeading_nl']!="")
-			{
-				$obj->setHeading($item['Eigenschap-3AHeading_nl']);
-			}
-			elseif($item['Eigenschap-3AHeading_en']!="")
-			{
-				$obj->setHeading($item['Eigenschap-3AHeading_en']);
-			}
-			else
-			{
-				$name_array=preg_split("/URIResolver\//", self::decodeerSMWNaam($item['@id']));
-				$obj->setHeading($name_array[1]);
-			}
-
-			foreach ($item as $key => $value)
-			{
-				if(!empty($value))
+				// Bepaal type IE
+				switch($item['Eigenschap-3AIntentional_Element_type'])
 				{
-					switch($key)
+					case 'Activity':
+						$obj=new Activity($item['@id']);
+						break;
+					case 'Outcome':
+						$obj=new Outcome($item['@id']);
+						break;
+					case 'Goal':
+						$obj=new Goal($item['@id']);
+						break;
+					case 'Belief':
+						$obj=new Belief($item['@id']);
+						break;
+					case 'Condition':
+						$obj=new Condition($item['@id']);
+						break;
+					default:
+						$obj = new IntentionalElement($item['@id']);
+				}
+
+				if($item['Eigenschap-3AHeading_nl']!="")
+				{
+					$obj->setHeading(uri::decodeerHaakjes($item['Eigenschap-3AHeading_nl']));
+				}
+				elseif($item['Eigenschap-3AHeading_en']!="")
+				{
+					$obj->setHeading(uri::decodeerHaakjes($item['Eigenschap-3AHeading_en']));
+				}
+				else
+				{
+					$obj->setHeading(Uri::SMWuriNaarLeesbareTitel($item['@id']));
+				}
+
+				foreach ($item as $key => $value)
+				{
+					if(!empty($value))
 					{
-						case 'Eigenschap-3AIntentional_Element_decomposition_type':
-							$obj->setDecompositionType($value);
-							break;
-						case 'Eigenschap-3AProduces':
-							$produces[$item['@id']]=$value;
-							break;
-						case 'Eigenschap-3AConsumes':
-							$consumes[$item['@id']]=$value;
-							break;
-						case 'Eigenschap-3AContext':
-							if(!$ie_context[$item['@id']])
-								$ie_context[$item['@id']]=array();
-							if (is_array($value))
-							{
-								$ie_context[$item['@id']]=array_merge($ie_context[$item['@id']],$value);
-							}
-							else
-							{
-								$ie_context[$item['@id']][]=$value;
-							}
-							break;
-						case 'Eigenschap-3APart_of':
-							$partOf[$item['@id']]=$value;
-							break;
-						case 'Eigenschap-3AInstance_of':
-							$instanceOf[$item['@id']]=$value;
-							break;
-						default:
+						switch($key)
+						{
+							case 'Eigenschap-3AIntentional_Element_decomposition_type':
+								$obj->setDecompositionType($value);
+								break;
+							case 'Eigenschap-3AProduces':
+								$produces[$item['@id']]=$value;
+								break;
+							case 'Eigenschap-3AConsumes':
+								$consumes[$item['@id']]=$value;
+								break;
+							case 'Eigenschap-3AContext':
+								if (is_array($value))
+								{
+									$ie_context[$item['@id']]=$value;
+								}
+								else
+								{
+									$ie_context[$item['@id']]=array($value);
+								}
+								break;
+							case 'Eigenschap-3APart_of':
+								$partOf[$item['@id']]=$value;
+								break;
+							case 'Eigenschap-3AInstance_of':
+								$instanceOf[$item['@id']]=$value;
+								break;
+							default:
+						}
 					}
 				}
+				$items[$item['@id']] = $obj;
 			}
-			$items[$item['@id']] = $obj;
 		}
-
 		/**
 		 * Maak voor alle mee te nemen Contexten een object aan
 		 */
 		$contexten=array();
 		foreach($alle_te_doorzoeken_uris as $context_uri)
 		{
+			$context_uri=Uri::deescape_uri($context_uri);
 			$nieuwecontext=new Context($context_uri);
-			$description_query='SELECT ?description WHERE { <'.$context_uri.'> <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ADescription> ?description }';
-			$description_result=$connectie->JSONQueryAsPHPArray($description_query);
-			$description=$description_result['results']['bindings'][0]['description']['value'];
-
-			if ($description!="")
-			{
-				$nieuwecontext->setDescription($description);
-			}
-			else
-			{
-				$nieuwecontext->setDescription(self::decodeerSMWNaam($context_uri));
-			}
+			$nieuwecontext->setDescription(self::geefContextbeschrijving($context_uri));
 			$contexten[$context_uri]=$nieuwecontext;
 		}
-
-		//echo '<pre>';
-		//var_dump($contexten);
-		//echo'</pre>';
 
 		/**
 		 * Leg de verbanden tussen de Contexten
 		 */
 		foreach($contexten as $context_uri => $context_object)
 		{
-			$supercontexten=$connectie->JSONQueryAsPHPArray('DESCRIBE ?supercontext WHERE { <'.$context_uri.'> <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ASupercontext> ?supercontext}');
+			$supercontexten=$connectie->JSONQueryAsPHPArray('DESCRIBE ?supercontext WHERE { '.Uri::escape_uri($context_uri).' property:Supercontext ?supercontext}');
+
 			if(array_key_exists('@graph',$supercontexten)) // Meerdere resultaten
 			{
 				foreach($supercontexten['@graph'] as $supercontext)
@@ -195,9 +185,6 @@ class JSON_EMontParser
 				}
 			}
 		}
-		//echo '<pre>';
-		//var_dump($contexten);
-		//echo'</pre>';
 
 		foreach ($items as $uri => $item)
 		{
@@ -241,94 +228,60 @@ class JSON_EMontParser
 			}
 			catch(Exception $e) {}
 
-
-			try
+			if(array_key_exists($uri,$ie_context))
 			{
-				if(array_key_exists($uri,$ie_context))
+				foreach($ie_context[$uri] as $context_uri)
 				{
-					//echo 'key bestaat';
-					foreach($ie_context[$uri] as $context)
+					try
 					{
-						//echo 'dump:';
-						//var_dump($context);
-						//echo 'context-uri:'.$context;
-						//var_dump($contexten[$context]);
-						@$item->addContext($contexten[$context]);
-						@$items[$uri]=$item;
+						@$item->addContext($contexten[$context_uri]);
+					}
+					catch(Exception $e) {}
+				}
+				@$items[$uri]=$item;
+			}
+
+			$query='DESCRIBE ?s WHERE { ?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3AElement_back_link> '.Uri::escape_uri($uri).' }';
+			$resultaat=$connectie->JSONQueryAsPHPArray($query);
+
+			if(array_key_exists('@graph',$resultaat))
+			{
+				foreach($resultaat as $deelresultaat)
+				{
+					if(array_key_exists(0,$deelresultaat))
+					{
+						foreach($deelresultaat as $koppeling)
+						{
+							$items[$uri]=self::verwerkKoppeling($koppeling,$items[$uri],$items);
+						}
 					}
 				}
 			}
-			catch(Exception $e) {}
-
-			$query='DESCRIBE ?s WHERE { ?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3AElement_back_link> <'.$uri.'> }';
-			$koppeling=$connectie->JSONQueryAsPHPArray($query);
-
-			try
+			else
 			{
-				$object=$items[$uri];
-
-				switch(@$koppeling['Eigenschap-3AElement_link_type'])
-				{
-					case 'Depends':
-						$koppelobject=new Depends();
-						@$koppelobject->setLinkNote($koppeling['Eigenschap-3AElement_link_note']);
-						@$koppelobject->setLink($items[$koppeling['Eigenschap-3AElement_link']]);
-						$object->addDepends($koppelobject);
-						break;
-					case 'Connects':
-						$koppelobject=new Connects();
-						@$koppelobject->setLinkNote($koppeling['Eigenschap-3AElement_link_note']);
-						@$koppelobject->setLinkCondition($koppeling['Eigenschap-3AElement_condition']);
-						@$koppelobject->setConnectionType($koppeling['Element_connection_type']);
-						@$koppelobject->setLink($items[$koppeling['Eigenschap-3AElement_link']]);
-						$object->addConnects($koppelobject);
-						break;
-					case 'Contributes':
-						$koppelobject=new Contributes();
-						@$koppelobject->setLinkNote($koppeling['Eigenschap-3AElement_link_note']);
-						@$koppelobject->setContributionValue($koppeling['Eigenschap-3AElement_contribution_value']);
-						@$koppelobject->setLink($items[$koppeling['Eigenschap-3AElement_link']]);
-						$object->addContributes($koppelobject);
-						break;
-					default:
-						break;
-				}
-
-				$items[$uri]=$object;
-			}
-			catch(Exception $e)
-			{
-				// Link valt vermoedelijk buiten de Context
+				$items[$uri]=self::verwerkKoppeling($resultaat,$items[$uri],$items);
 			}
 		}
 		return array_merge($items,$contexten);
 	}
 
-	/**
-	 * Zet een in SMW opgeslagen naam van een Mediawiki-artikel om in een leesbare variant.
-	 * Bijvoorbeeld: Eigenschap-3AElement_link_type naar Eigenschap: Element link type
-	 */
-	static function decodeerSMWNaam($naam)
-	{
-		// ID's in RDF-store staan in procentnotatie, maar met een - ipv een %. Dit stukje code zorgt voor
-		// een correcte procentnotatie en zet die vervolgens om naar de bedoelde tekens.
-		return urldecode(strtr($naam, "-_","% "));
-	}
+
 
 	/**
 	 * Staat hier omdat het op URL gaat.
 	 */
 	static function isSituatie($context_uri)
 	{
+		$context_uri=Uri::escape_uri($context_uri);
 		$query="DESCRIBE ?s ?o WHERE {
-			?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ASelection_link> <".$context_uri.">
+			?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ASelection_link> ".$context_uri."
 			.
 			?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3APractice_back_link> ?o
 			}";
 		$connectie=new SPARQLConnection();
 		$result=$connectie->JSONQueryAsPHPArray($query);
 
-		if (empty($result)) // Leeg resultaat
+		if (empty($result['@graph'])) // Leeg resultaat
 		{
 			return false;
 		}
@@ -344,8 +297,9 @@ class JSON_EMontParser
 	static function zoekSubrollen($context_uri)
 	{
 		$subrollen=array();
+		$context_uri=Uri::escape_uri($context_uri);
 
-		$query='DESCRIBE ?context WHERE { ?context <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ASupercontext> <'.$context_uri.'> }';
+		$query='DESCRIBE ?context WHERE { ?context <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ASupercontext> '.$context_uri.' }';
 		$connectie=new SPARQLConnection();
 		$contexten=$connectie->JSONQueryAsPHPArray($query);
 
@@ -361,8 +315,114 @@ class JSON_EMontParser
 				}
 			}
 		}
-
 		return $subrollen;
+	}
+
+	/**
+	 * Geeft lijst van L1-modellen (situaties)
+	 * TODO: Op een geschiktere plek onderbrengen.
+	 */
+	static function geefL1modellen()
+	{
+		$query='DESCRIBE ?context WHERE {?context <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Categorie-3AContext>}';
+		$connectie=new SPARQLConnection();
+		$result=$connectie->JSONQueryAsPHPArray($query);
+
+		if(isset($result['@graph']))
+		{
+			$contexten=array();
+
+			foreach($result['@graph'] as $item)
+			{
+				if(self::isSituatie($item['@id']))
+				{
+					$context=new Context($item['@id']);
+					$context->setDescription(self::geefContextbeschrijving($item['@id']));
+					$contexten[$item['@id']]=$context;
+				}
+			}
+
+			return $contexten;
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+
+	static function geefContextbeschrijving($context_uri)
+	{
+		$connectie=new SPARQLConnection();
+		$description_query='SELECT ?description WHERE { '.Uri::escape_uri($context_uri).' <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ADescription> ?description }';
+		$description_result=$connectie->JSONQueryAsPHPArray($description_query);
+		$description=$description_result['results']['bindings'][0]['description']['value'];
+
+		if ($description!="")
+		{
+			return $description;
+		}
+		else
+		{
+			return Uri::SMWuriNaarLeesbareTitel($context_uri);
+		}
+	}
+
+	static function verwerkKoppeling($koppeling,&$object,&$objectenpool)
+	{
+		try{
+			if(is_array($koppeling) && $objectenpool[$koppeling['Eigenschap-3AElement_link']])
+			{
+				switch(@$koppeling['Eigenschap-3AElement_link_type'])
+				{
+					case 'Depends':
+						$koppelobject=new Depends();
+						@$koppelobject->setLinkNote($koppeling['Eigenschap-3AElement_link_note']);
+						@$koppelobject->setLink($objectenpool[$koppeling['Eigenschap-3AElement_link']]);
+						$object->addDepends($koppelobject);
+						break;
+					case 'Connects':
+						$koppelobject=new Connects();
+						@$koppelobject->setLinkNote($koppeling['Eigenschap-3AElement_link_note']);
+						@$koppelobject->setLinkCondition($koppeling['Eigenschap-3AElement_condition']);
+						@$koppelobject->setConnectionType($koppeling['Eigenschap-3AElement_connection_type']);
+						@$koppelobject->setLink($objectenpool[$koppeling['Eigenschap-3AElement_link']]);
+						$object->addConnects($koppelobject);
+						break;
+					case 'Contributes':
+						$koppelobject=new Contributes();
+						@$koppelobject->setLinkNote($koppeling['Eigenschap-3AElement_link_note']);
+						@$koppelobject->setContributionValue($koppeling['Eigenschap-3AElement_contribution_value']);
+						@$koppelobject->setLink($objectenpool[$koppeling['Eigenschap-3AElement_link']]);
+						$object->addContributes($koppelobject);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+		catch(Exception $e) {}
+		return $object;
+	}
+
+	static function isPractice($context_uri)
+	{
+		//$context_uri=Uri::escape_uri($context_uri);
+		$query="DESCRIBE ?s ?o WHERE {
+			?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3ASelection_link> ".$context_uri."
+			.
+			?s <http://127.0.0.1/mediawiki/mediawiki/index.php/Speciaal:URIResolver/Eigenschap-3APractice_back_link> ?o
+			}";
+		$connectie=new SPARQLConnection();
+		$result=$connectie->JSONQueryAsPHPArray($query);
+
+		if (empty($result['@graph'])) // Leeg resultaat
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
 	}
 }
 ?>
